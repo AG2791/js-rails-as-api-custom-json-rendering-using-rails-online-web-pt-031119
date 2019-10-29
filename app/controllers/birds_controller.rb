b942de5c13de82b65b77469e1cae918270f0ab0c
AJ Guiles <ashiguiles@gmail.com>
2019-10-29 02:58:09 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -8,7 +8,7 @@ class BirdsController < ApplicationController
   #   bird = Bird.find_by(id: params[:id])
   #   render json: bird
   # end
-  Filtering out unwanted data 
+  ##Filtering out unwanted data 
   def show
   bird = Bird.find_by(id: params[:id])
   render json: {id: bird.id, name: bird.name, species: bird.species } 
